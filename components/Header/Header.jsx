12fbf8f9c20cb644593c92aba840d661f907e39b
Alireza Aberoumand <alirezaaberoumand7192@gmail.com>
2024-06-26 21:10:28 -0700

    add side bar menu

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import "./Header.css"
 import { FaHamburger } from "react-icons/fa";
+import { MdOutlineClose } from "react-icons/md";
 
 function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const sideBarMenu = useRef()
+    let hamburgerMenuHandler = (param) => {
+        setIsMenuOpen(param);
+        // console.log(sideBarMenu.current.style.left = "0px")
+    }
+
+    useEffect(() => {
+        console.log(isMenuOpen)
+        if (!isMenuOpen) {
+            sideBarMenu.current.style.left = "-254.8px";
+        } else {
+            sideBarMenu.current.style.left = "0px";
+        }
+    }, [isMenuOpen])
+
     return (
         <>
             <header className='project-main-container'>
@@ -13,27 +30,27 @@ function Header() {
                     <div className="header-navigation-container">
                         <ul>
                             <li>
-                                <a href="">
-                                    Homepage
+                                <a href="#">
+                                    Home Page
                                 </a>
                             </li>
                             <li>
-                                <a href="">
+                                <a href="#">
                                     Menu
                                 </a>
                             </li>
                             <li>
-                                <a href="">
+                                <a href="#">
                                     Promotions
                                 </a>
                             </li>
                             <li>
-                                <a href="">
+                                <a href="#">
                                     News
                                 </a>
                             </li>
                             <li>
-                                <a href="">
+                                <a href="#">
                                     Contact
                                 </a>
                             </li>
@@ -44,16 +61,24 @@ function Header() {
                         <button>Log in</button>
                     </div>
                     <div className="burger-container">
-                        <FaHamburger className='burger-icon' />
+                        {
+                            !isMenuOpen
+                                ? <FaHamburger className='burger-menu-icon burger-icon' onClick={() => hamburgerMenuHandler(true)} />
+                                : <MdOutlineClose className='burger-menu-icon close-icon' onClick={() => hamburgerMenuHandler(false)} />
+                        }
                     </div>
                 </div>
+                <div className="side-menu-container" ref={sideBarMenu}>
+                    <ul>
+                        <li><a href="#">Home Page</a></li>
+                        <li><a href="#">Menu</a></li>
+                        <li><a href="#">Promotions</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
             </header>
-            <main>
-
-            </main>
-            <footer>
-
-            </footer></>
+        </>
     )
 }
 
